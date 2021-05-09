72764f43cf000554eaf88261543c3de23dd40297
zidanefst <zidanefst@gmail.com>
2021-05-09 15:49:44 +0100

    removing old comments

--- js/main.js
+++ js/main.js
@@ -1,33 +1,3 @@
-/* function preloaderFadeOutInit(){
-    $('.preloader').fadeOut('slow');
-    $('body').attr('id','');
-}
-
-// Window load function
-jQuery(window).on('load', function () {
-    (function ($) {
-        preloaderFadeOutInit();
-    })(jQuery);
-}); */
-
-//Button Send Message
-
-//Success
-/* $('button').click(function() {
-  $(this).toggleClass('success');
-  $('button p').text(function(i, text) {
-    return text === "Email Me" ? "Email Me" : "Email Me";
-  });
-}); */
-
-//Error
-/* $('button').click(function() {
-  $(this).toggleClass('error');
-  $('button p').text(function(i, text) {
-    return text === "Try again!" ? "Send" : "Try again!";
-  });
-}); */
-
 $('a[href*=\\#]').on('click', function(event){     
     event.preventDefault();
     $('html,body').animate({scrollTop:$(this.hash).offset().top-100}, 500);
@@ -42,7 +12,6 @@ if($(window).width() < 1024)
 }
 
 /* TODO menu item active changes style to bold on scroll */
-
 
 /* menuham is active class activation */
 
